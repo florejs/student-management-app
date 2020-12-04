d5917ba02e4a01b95704aa5f9063fc9030addf5c
florejs <60922048+florejs@users.noreply.github.com>
2020-12-04 11:07:47 +0900

    change email

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import * as actions from 'actions'
 
 const Login = ({dispatch, location}) => {
-  const [email, setEmail] = useState('new@gmail.com')
+  const [email, setEmail] = useState('contact@ecomwarrioracademy.com')
   const [password, setPassword] = useState('password')
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
